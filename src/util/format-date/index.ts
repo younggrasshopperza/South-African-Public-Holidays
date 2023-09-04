e1e134b18836e9f20041a20e68d32ee6edcb3975
Alex Sexwale <alexsexwale@gmail.com>
2023-09-04 17:05:46 +0200

    update public holidays to be on a Monday if the day is on a Sunday

--- src/util/format-date/index.ts
+++ src/util/format-date/index.ts
@@ -1,4 +1,7 @@
-export const formatDate = (date: Date) => {
+export const formatDate = (date: Date): string => {
+    if (date.getDay() === 0) { // If Sunday
+        date.setDate(date.getDate() + 1); // Move to the next day
+    }
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const dd = String(date.getDate()).padStart(2, '0');
